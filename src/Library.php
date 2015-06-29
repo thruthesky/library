29bc191a2a1ca76b86c65a8960992bfdcadf35f1
JaeHo Song <thruthesky@gmail.com>
2015-06-29 22:47:37 +0900

    work for post modules

--- src/Library.php
+++ src/Library.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ *
+ * https://docs.google.com/document/d/1koxonGQl20ER7HZqUfHd6L53YXT5fPlJxCEwrhRqsN4/edit#
+ *
+ */
 namespace Drupal\library;
 use Drupal\user\Entity\User;
 
@@ -11,8 +16,6 @@ use Drupal\user\Entity\User;
  */
 
 class Library {
-
-
 
   const ERROR_PLEASE_LOGIN_FIRST = 'ERROR_PLEASE_LOGIN_FIRST';
   const ERROR_USER_EXISTS = 'ERROR_USER_EXISTS';
@@ -40,8 +43,9 @@ class Library {
         '11'=>'November',
         '12'=>'December'
     ];
-  
-  public static function getThemeName() {
+    private static $count_log = 0;
+
+    public static function getThemeName() {
     $uri = \Drupal::request()->getRequestUri();
 
       // di($uri);
@@ -61,7 +65,9 @@ class Library {
   }
 
   public static function isFromSubmit() {
-    return \Drupal::request()->get('mode') == 'submit';
+      if ( \Drupal::request()->get('mode') == 'submit' ) return true;
+      if ( \Drupal::request()->get('submit') ) return true;
+      return false;
   }
 
 
@@ -178,13 +184,15 @@ class Library {
    */
   public static function getInput() {
 
-    if ( empty(self::$input) ) {
-      $request = \Drupal::request();
-      $get = $request->query->all();
-      $post = $request->request->all();
-      self::$input = array_merge( $get, $post ); 
-    }
-    return self::$input;
+      if ( empty(self::$input) ) {
+          $request = \Drupal::request();
+          $get = $request->query->all();
+          $post = $request->request->all();
+          self::$input = array_merge( $get, $post );
+      }
+      if ( !isset(self::$input['page_no']) ) self::$input['page_no'] = 1;
+
+      return self::$input;
   }
 
 
@@ -235,7 +243,7 @@ class Library {
      * @param array $info
      * @return array
      * @code
-     * return x::error(-1234, ['message'=>'This is error', 'name'=>'JaeHo Song']);
+     * if( empty( $name ) ) return Library::error(-1, Language::string('library', 'empty_category_name'));
      * @endcode
      */
   public static function error($code, $info = null) {
@@ -253,13 +261,15 @@ class Library {
   /**
    * Returns true if the input object indicates Error.
    *
-   * @note if $re is minus or $re[0] is minus, then it considers as error.
+   * @note
    *
-   * @param $re
+   * @param $re -
+   *    - true if $re is less than 0
    * @return bool
    */
   public static function isError($re) {
-      return $re;
+      if ( is_numeric($re) && $re < 0 ) return true;
+      return false;
   }
   
   
@@ -312,9 +322,11 @@ class Library {
      * @return mixed
      */
   public static function loginUser($username) {
-        $user = user_load_by_name($username);
-        user_login_finalize( $user );
-        return $user->id();
+      $user = user_load_by_name($username);
+      if ( $user ) {
+          user_login_finalize( $user );
+          return $user->id();
+      }
   }
 
 
@@ -351,6 +363,290 @@ class Library {
         return $ln;
     }
 
+
+
+    /**
+     * @param $str
+     * @TODO @WARNING If the log file permission is not open to public, then it will create error.
+     */
+    public static function log ( $str )
+    {
+        /**
+         * @TODO @WARNING This was a bug. If $str is unset/empty, there will be a warning error on Drupal.
+         */
+        if ( empty($str) ) return;
+
+        $path_log = "./debug.log";
+        $fp_log = fopen($path_log, 'a+');
+        if ( ! is_string($str) ) {
+            $str = print_r( $str, true );
+        }
+        self::$count_log ++;
+        fwrite($fp_log, self::$count_log . " : $str\n");
+
+        fclose( $fp_log );
+    }
+
+
+    /**
+     *
+     * Returns HTML elements for displaying page navigation
+     * @param $page_no - current page number
+     * @param $total_record - the whole number of item
+     * @param $items_per_page - items per page
+     * @param $qs - Extra query string.
+     *      - If you want to add some http query.
+     *      - Query string
+     * @param int $paging - is the number of pages in navigation. How many pages you want to show in the navigation bar.
+     * @param array $text - Texts on navigation bar.
+     *        - This is the text on the navigation bar.
+     *      - You can set a text for first page, previous page, next page, last page and so on.
+     *      - ex) $text = array("First ", "Previous (n)", "Next (n)", "Last");
+     *          -- Where '(n)' is number of actual page.
+     * @param $path - is the page of the index.php
+     * @return string
+     * @code How to display list and navigation bar.
+     *      // Get current page no
+     *      if ( $in[page_no] ) $page_no = $in[page_no];
+     *      else $page_no = 1;
+     *      // Get number of item to show in one page.
+     *      $items_per_page = 15;
+     * // Get total number of records.
+     *      $total_record = "전체 레코드(글, 게시물, 쪽지 수)";
+     *      // How to get the list from database.
+     *      $rows = mysql_query("select * from table_name order by UID desc limit " . ($page_no - 1) * $items_per_page . ", ". $items_per_page, $connect);
+     * // display content...
+     *      // display navigation bar like below.
+     *      echo paging( $page_no, $total_record, $items_per_page );
+     * @endcode
+     *
+     *
+     *
+     * @note If you omit $qs, $_GET['page_no'] will be deleted and set new page no.
+     * @note How to design
+     *      - You can edit style with CSS Style Overriding
+     *      - Each block has a class='cell'
+     *      - ex) .navigator .cell { ... }
+     *
+     *
+     * @note CSS classes
+     *
+     * - nav.navigation-bar - is the wrapper
+     *      - class='first_page' - is the first page.
+     *      - class='previous_page' - is the previous page.
+     *      - class='page_no' - is each page.
+     *      - class='selected' - is the current page.
+     *      - class='next_page' - is next page.
+     *      - class='last_page' '- is last page.
+     *
+     * @note Sample CSS Code
+        nav.navigation-bar a {
+            display:inline-block;
+            margin:0 1px;
+            padding:4px 6px;
+            background-color: #d3e8f4;
+            border-radius: 2px;
+        }
+     */
+    public static function paging( $page_no, $total_record, $items_per_page, $qs=NULL, $paging=10, $text=null, $path=null) {
+        if ( empty($total_record) ) return NULL;
+        if ( empty($text) ) $text = array("&lt;&lt;", "Previous (n)", "Next (n)", "&gt;&gt;");
+
+
+
+        /// If you uncomment below, "[ 1 ]" will not appear on a list where there is only 1 page.
+        /// if ( $total_record <= $items_per_page ) return NULL;
+
+
+
+
+        if ( empty($qs) ) {
+            /// @warning when the input of $qs is empty,
+            /// it returns the value of HTTP input excep page_no and idx
+            /// and it puts $in[action]='list'
+            $qv = Library::input();
+            unset($qv["page_no"]);
+            unset($qv["id"]);
+            $qs = http_build_query($qv);
+        }
+
+
+        // Number pages to show in navigation bar.
+        // Default number of pages is 10.
+        if ( !$paging ) $paging = 10;
+
+        // Number of pages to display on navigatio bar.
+        $text[1] = str_replace("(n)", $paging, $text[1]);
+        $text[2] = str_replace("(n)", $paging, $text[2]);
+
+
+        // Total number of pages.
+        if ( $total_record % $items_per_page != 0 ) {
+            $totalpage = intval($total_record / $items_per_page) + 1;
+        } else {
+            $totalpage = intval($total_record / $items_per_page);
+        }
+
+
+
+
+
+        // Page number that begins on the navigation bar.
+        if ( $page_no % $paging == 0 ) {
+            $startpage = $page_no - ( $paging - 1 );
+        } else {
+            $startpage = intval( $page_no / $paging ) * $paging + 1;
+        }
+
+
+        // Prvious Page number
+        $prevpage = $startpage - 1;
+
+        // Next Page number
+        $nextpage = $startpage + $paging;
+
+        // Lst Page number
+        if ( $totalpage / $paging > 1 ) {
+            $laststartpage = (intval($totalpage / $paging) * $paging ) + 1;
+        } else {
+            $laststartpage = 1;
+        }
+
+
+        $rt = "<nav class='navigation-bar'>";
+
+        /** @short first page button.
+         *
+         * @note If the text is empty, it does not show the first page button.
+         */
+        $first_page = "$text[0]";
+        if ( $first_page && $page_no > $paging ) {
+
+            if ( $qs ) {
+                $rt .= "<a class='first-page' href='$path?page_no=1&".$qs."'>";
+            } else {
+                $rt .= "<a class='first-page' href='$path?page_no=1'>";
+            }
+            $rt .= "$first_page</a>";
+        } else {
+
+        }
+
+
+
+
+        // Previous page button
+        $previous_page = "$text[1]";
+        if ( $totalpage > $paging && $page_no > $paging ) {
+            if ( $qs ) {
+                $rt .= "<a class='prev-page' href='$path?page_no=".$prevpage."&".$qs."'>";
+            } else {
+                $rt .= "<a class='prev-page' href='$path?page_no=".$prevpage."'>";
+            }
+            $rt .= "$previous_page</a>";
+        } else {
+        }
+
+
+
+        // This is list has only one(1) page?
+        if ( $totalpage <= 1 ) {
+            $rt .= "<div class='one-page'>1</div>";
+        }
+        else {
+            // Page number list.
+            for ( $i = $startpage ; $i <= ($startpage + ($paging - 1) ) ; $i++ ) {
+                // If this page number is not the current page,
+                if ( $page_no != $i ) {
+                    if ( $qs ) {
+                        $rt .= "<a class='page' href='$path?page_no=".$i."&".$qs."'>".$i."</a>";
+                    } else {
+                        $rt .= "<a class='page' href='$path?page_no=".$i."'>".$i."</a>";
+                    }
+                }
+                // If this page number is the current page,
+                else {
+                    $rt .= "<a class='page selected' href='javascript:void(0);'>".$i."</a>";
+                }
+
+                //
+                if ( $i >= $totalpage ) {
+                    break;
+                }
+            }
+        }
+
+        // 'Move to Next block' button
+        $next_page = "$text[2]";
+
+        if ( $startpage + $paging - 1 < $totalpage) {
+            if ( $qs ) {
+                $rt .= "<a class='next-page' href='$path?page_no=".$nextpage."&".$qs."'>";
+            } else {
+                $rt .= "<a class='next-page' href='$path?page_no=".$nextpage."'>";
+            }
+            $rt .= "$next_page</a>";
+        } else {
+
+        }
+
+
+        /** @short Move to last page button.
+         *
+         * @note If the text is empty, it does not show the last page button.
+         */
+        $last_page = "$text[3]";
+        if ( $last_page && $page_no < intval($laststartpage) ) {
+            if ( $qs ) {
+                $rt .= "<a class='last-page' href='$path?page_no=".$totalpage."&".$qs."'>";
+            } else {
+                $rt .= "<a class='last-page' href='$path?page_no=".$totalpage."'>";
+            }
+            $rt .= "$last_page</a>";
+        }
+        else {
+        }
+
+        $rt .= "</nav>";
+
+        return $rt;
+    }
+
+    public static function getPageNo() {
+        $page_no = \Drupal::request()->get('page_no');
+        if ( empty($page_no) ) $page_no = 1;
+        return $page_no;
+    }
+
+    public static function parseErrorMessage(&$variables, $error_html_twig=null) {
+        if ( empty($error_html_twig) ) $error_html_twig = "modules/library/templates/error.html.twig";
+        $template = @file_get_contents($error_html_twig);
+        if ( empty($template) ) return;
+        $error = self::getError();
+        $markup = \Drupal::service('twig')->renderInline($template, ['error'=>$error]);
+        $variables['error_message'] =  $markup;
+    }
+
+
+    public static function saveFormSubmit($form_name) {
+        self::setGroupConfig($form_name, Library::input());
+    }
+    public static function setGroupConfig($group_name, $arr) {
+        foreach( $arr as $k => $v ) {
+            \Drupal::state()->set("$group_name.$k", $v);
+        }
+    }
+    public static function getGroupConfig($group_name) {
+        $configs = getStateGroup($group_name);
+        if ( $configs ) {
+            foreach( $configs as $k => $v ) {
+                $k = str_replace("$group_name.", '', $k);
+                $news[$k] = $v;
+            }
+            return $news;
+        }
+        else return [];
+    }
 
 }
 
