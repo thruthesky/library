5fc3703ef038e3d2cf5f79998a7be4b02898729c
JaeHo Song <thruthesky@gmail.com>
2015-07-02 21:07:40 +0900

    work for library: code refactoring for global use

---
+++ js/global.js
@@ -0,0 +1,66 @@
+var $ = jQuery;
+$(function(){
+    $("body").on("click", ".api", function(){
+        var url = $(this).attr('url');
+        var selector = $(this).attr('selector');
+        var callback = $(this).attr('callback');
+
+        if ( typeof callback == 'function' ) {
+            console.log("callback: " + callback);
+            ajax_api(url, callback);
+        }
+        else if ( typeof selector != 'undefined' && selector != '' ) {
+            console.log("selector: " + selector);
+            ajax_api(url, function(re){
+                var data;
+                try {
+                    data = JSON.parse(re);
+                    if ( typeof data.error != 'undefined' ) alert(data.error);
+                    else $(selector).html(data.result);
+                }
+                catch (e) {
+                    console.log("JSON.parse() error");
+                    console.log(re);
+                }
+            });
+        }
+        else {
+            console.log("no callback and no selecotr");
+        }
+    });
+});
+
+/**
+ * ajax api call for portal
+ *
+ * @param qs - query string or POST data
+ * @param callback_function
+ *
+ * @code
+ var qs = {};
+ qs.method = 'scheduleTable';
+ qs.teacher = teacher;
+ ajax_api ( qs, function( re ) { } );
+
+ * @Attention This method saved the returned-data from server into Web Storage IF qs.cache is set to true.
+ *
+ *  - and it uses the stored data to display on the web browser,
+ *  - after that, it continues loading data from server
+ *  - when it got new data from server, it display onto web browser and update the storage.
+ *
+ */
+function ajax_api( url, callback_function )
+{
+    console.log('ajax_api:' + url);
+    var promise = $.ajax( { url : url } );
+    promise.done( function( o ) {
+        console.log("promise.done() : callback function : " + callback_function);
+        callback_function( o )
+    });
+
+    promise.fail( function( re ) {
+        // alert('ajax call - promise failed');
+        console.log("promise failed...");
+        console.log(re);
+    });
+}
