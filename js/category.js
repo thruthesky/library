3998210ba0c4491e626911d1e0dbc00bad181c7b
mark <mark@gmail.com>
2015-07-15 09:31:03 -0400

    fixes for category

--- js/category.js
+++ js/category.js
@@ -3,7 +3,7 @@ $(function(){
     var $body = $('body');
     $("body").on( "click",".category .button-wrapper .add", callback_category_add );
     $("body").on( "click",".category .button-wrapper .edit", callback_category_edit );
-    $("body").on( "click",".category .cancel", callback_category_cancel );
+    $("body").on( "click",".category .cancel", callback_category_cancel );	
 });
 
 function callback_category_add(){
@@ -15,10 +15,10 @@ function callback_category_add(){
     $this.remove();
 }
 
-function callback_category_edit(){
+function callback_category_edit(){	
     $this = $(this);
     var id = $this.attr("id");
-    var form = renderEditForm( id );
+    var form = renderEditForm( id );	
     $(".category[category-id='" + id + "'] .label .category-name").html( form );
     $(".category[category-id='" + id + "'] input[type='text']").select();
     $this.remove();
@@ -42,13 +42,13 @@ function callback_category_cancel(){
     window.location.reload();
 }
 
-function renderEditForm( id ){
+function renderEditForm( id ){	
     if( $(".category[category-id='" + id + "'] .label .category-name a").length ){
         var text = $(".category[category-id='" + id + "'] .label .category-name a").text();
     }
     else{
         var text = $(".category[category-id='" + id + "'] .label .category-name").text();
-    }
+    }	
     var markup	=	"<form class='form-update' action='/library/category/admin/group/update'>" +
         "<fieldset><div class='row'><div class='value'><div class='element'>" +
         "<input type='hidden' name='id' value='" + id + "'>" +
